21d1aaef329d9bef580c77558dfdf4d07b79bf79
Alicia Lu <lualicia88@gmail.com>
2024-06-17 19:46:19 +0000

    rewrite logging

--- log.py
+++ log.py
@@ -1,22 +1,36 @@
+class IterationLog():
 
-class Log:
-    def __init__(self, attention_log_freq, loss_log_freq):
-        self.attention_scores = []
-        self.mse_losses = []
-        self.iterations = []
+    def __init__(
+        self,
+        iteration,
+        attention,
+        train_loss,
+        valid_loss
+    ):
+        self.iteration = iteration
+        self.attention = attention
+        self.train_loss = train_loss
+        self.valid_loss = valid_loss
+
+class Log():
+
+    def __init__(self, attention_log_freq):
+
+        self.iteration_logs = []
         self.attention_log_freq = attention_log_freq
-        self.loss_log_freq = loss_log_freq
 
-    def log_iteration(self, num_iteration):
-        self.iterations.append(num_iteration)
-
-    def log_loss(self, loss):
-
-        if self.iterations[-1] % self.loss_log_freq == 0:
-            self.mse_losses.append(loss)
-
-    def log_attention(self, attention_score):
-
-        if self.iterations[-1] % self.attention_log_freq == 0:
-            self.attention_scores.append(attention_score)
-
+    def log_iteration(
+        self,
+        iteration,
+        attention=None,
+        train_loss=None,
+        valid_loss=None
+    ):
+        self.iteration_logs.append(
+            IterationLog(
+                iteration,
+                attention,
+                train_loss,
+                valid_loss
+            )
+        )
