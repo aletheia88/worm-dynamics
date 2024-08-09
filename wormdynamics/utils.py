6310fb170ec704161d03e033cab3d055de6899c0
Alicia <lualicia88@gmail.com>
2024-08-09 14:43:37 +0000

    fix bug

--- wormdynamics/utils.py
+++ wormdynamics/utils.py
@@ -1,14 +1,15 @@
+from info import *
 from tqdm import tqdm
+import glob
 import json
 import numpy as np
-import glob
 
 
 def filter_pumping_datasets():
     """ Filter datasets to retain only those where the difference between the 
     75th percentile and the 25th percentile of the pumping rate exceeds 0.5. """
 
-    dataset_paths = glob.glob(f"/storage/fs/store1/alicia/transformer/all")
+    dataset_paths = glob.glob(f"/storage/fs/store1/alicia/transformer/all/*.json")
     filtered_datasets = []
 
     for dataset_path in tqdm(dataset_paths):
@@ -17,7 +18,9 @@ def filter_pumping_datasets():
             data = json.load(f)
             pumping_rates = np.array(data["pumping"], dtype=np.float32)
 
-        if np.percentile(pumping_rates, 75) - np.percentile(pumping_rates, 25):
+        if any(dataset in dataset_path for dataset in HEATSTIM):
+            continue
+        if np.percentile(pumping_rates, 75) - np.percentile(pumping_rates, 25) > 0.5:
             filtered_datasets.append(dataset_path)
 
-filter_pumping_datasets()
+    return filtered_datasets
