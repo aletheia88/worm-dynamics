d53a17409a3db22a175428f769c172c896f62b42
Alicia <lualicia88@gmail.com>
2024-10-10 15:10:57 +0000

    fix: ensure same number of unique samples for each dataset

--- src/attention_predict/benchmark.py
+++ src/attention_predict/benchmark.py
@@ -33,10 +33,12 @@ def evaluate(
             window_size=window_size,
             device=device,
             slices=slice(0, max_length))
+    print(f'dataset length: {len(dataset)}')
     dataloader = torch.utils.data.DataLoader(
             dataset,
             batch_size=1,
-            shuffle=True) # shuffle to get random windows per worm
+            shuffle=True,
+            drop_last=False) # shuffle to get random windows per worm
 
     # mse_per_dataset = {
     # '2022-06-14-13': {
@@ -44,34 +46,57 @@ def evaluate(
     #   1: [mse1, mse2, ..., mse100] # column 1 reconstruction errors
     #   2: [mse1, mse2, ..., mse100]} # column 2 reconstruction errors
     # }
+    # find datasets that have ground truth for all columns
+    datasets_with_gt = get_datasets_with_all_ground_truth(data_path, dataset_path,
+                                                          num_neurons)
     mse_per_dataset = {ds: {col: [] for col in range(num_inputs)}
-                       for ds in dataset.datasets}
+                       for ds in datasets_with_gt}
+    sample_metainfo = {ds: [] for ds in datasets_with_gt}
 
-    for _, (inputs, worm, start_frame, end_frame, ds) in enumerate(dataloader):
+    for i, (inputs, worm, start_frame, end_frame, dataset) in enumerate(dataloader):
 
-        if len(mse_per_dataset[ds[0]][0]) >= num_samples:
-            break
+        ds = dataset[0]
 
-        worm_idx = worm.item()
-        targets = deepcopy(inputs)
+        if ds not in datasets_with_gt:
+            continue
 
-        # ensure that masked data all have ground truth to compute MSE
-        ground_truth_available = True
-        for i in range(num_neurons):
-            if (torch.max(targets[0, i, :]).item() == 0 and
-                torch.min(targets[0, i, :]).item() == 0):
-                ground_truth_available = False
+        samples_filled = True
+        for ds in datasets_with_gt:
+            if len(mse_per_dataset[ds][0]) < num_samples:
+                samples_filled = False
                 break
-        if not ground_truth_available:
-            break
 
+        if samples_filled:
+            return mse_per_dataset, sample_metainfo
+
+        targets = deepcopy(inputs)
         inputs[:, mask_indices, :] = 0
         outputs = model(inputs)
-        for i in range(num_inputs):
-            mse_per_dataset[ds[0]][i].append(
-                    reconstruction_loss(targets[0, i, :], outputs[0, i, :]))
 
-    return mse_per_dataset
+        sample_metainfo[ds].append((start_frame.item(), end_frame.item()))
+        for i in range(num_inputs):
+            mse_per_dataset[ds][i].append(
+                    reconstruction_loss(targets[0, i, :], outputs[0, i, :]).item())
+
+
+def get_datasets_with_all_ground_truth(data_path, dataset_path, num_neurons):
+
+    data = np.load(data_path)
+    datasets = np.load(dataset_path)
+    filtered_datasets = []
+
+    for i, dataset in enumerate(datasets):
+
+        ground_truth_available = True
+        for j in range(num_neurons):
+            if (np.max(data[i, j, :]) == 0 and np.min(data[i, j, :]) == 0):
+                ground_truth_available = False
+                break
+
+        if ground_truth_available:
+            filtered_datasets.append(dataset)
+
+    return filtered_datasets
 
 
 if __name__ == "__main__":
@@ -95,11 +120,18 @@ if __name__ == "__main__":
     data_path = f'{base}/data/{ds_name}_test.npy'
     dataset_path = f'{base}/data/{ds_name}_test_ds.npy'
 
-    evaluate(
+    mse_per_dataset, sample_metainfo = evaluate(
         model,
         model_ckpt_path,
         mask_indices,
         data_path,
         dataset_path,
         num_inputs,
-        num_neurons)
+        num_neurons,
+        device=device)
+    for ds, mse_loss in mse_per_dataset.items():
+        print(f'{ds}: {len(mse_loss[0])}')
+    print('Number of unique frame chunks')
+    for ds, frame_chunk in sample_metainfo.items():
+        unique_frame_chunk = np.unique(np.array(frame_chunk), axis=1)
+        print(f'{ds}: {unique_frame_chunk.shape[0]}')
