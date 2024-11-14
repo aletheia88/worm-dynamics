92a1eb53e701b524505a3596589098622a02ee13
Alicia <lualicia88@gmail.com>
2024-11-14 16:53:55 +0000

    simplify masking and loss computation

--- experiments/run_hybrid_net.py
+++ experiments/run_hybrid_net.py
@@ -54,31 +54,25 @@ def train(
             num_samples = targets.shape[0]
             # find which neurons are recorded per sample in a batch
             recorded_neuron_indices = get_recorded_neuron_indices(targets, num_neurons)
-            # apply masking to each sample according to which neurons are recorded
-            # mask_indices = get_mask_indices(
-            #         num_inputs,
-            #         recorded_neuron_indices,
-            #         neuron_indices)
+            # mask unrecoreded neuro with zeros
+            mask_indices = get_mask_indices(
+                    num_inputs,
+                    recorded_neuron_indices,
+                    neuron_indices)
 
-            ### only masking out all neurons
-            mask_indices = {n: neuron_indices for n in range(num_samples)}
+            ### only masking out the missing neurons
             for n in mask_indices.keys():
                 inputs[n, mask_indices[n], :] = 0
 
             optimizer.zero_grad()
             outputs, attention_weights = model(inputs)
-            # compute MSE loss for each sample and sum up
-            print(f'attn: {attention_weights}')
-            print(f'outputs: {outputs}')
 
-            loss = aggregate_loss_v1(
+            # compute MSE loss for each sample and sum up
+            loss = aggregate_loss(
                     targets,
                     outputs,
                     recorded_neuron_indices,
-                    behavior_indices,
-                    mask_indices,
                     reconstruction_loss)
-            print(f'loss: {loss}')
             loss.backward()
             optimizer.step()
             loss_average += loss.item()
@@ -109,12 +103,25 @@ def ensure_dir_exists(directories):
             os.makedirs(directory)
 
 
+def get_mask_indices(
+    num_inputs,
+    recorded_neuron_indices,
+    neuron_indices,
+):
+    """ Masking out the missing neurons """
+    mask_indices = {}
+
+    for n in recorded_neuron_indices.keys():
+        mask_indices[n] = list(set(neuron_indices) - set(recorded_neuron_indices[n]))
+
+    return mask_indices
+
+
 def aggregate_loss(
-        targets,
-        outputs,
-        recorded_neuron_indices,
-        behavior_indices,
-        reconstruction_loss
+    targets,
+    outputs,
+    recorded_neuron_indices,
+    reconstruction_loss
 ):
     """ Compute MSE loss for each batch separately on the reconstruction of recorded
     neural and behavioral activities. """
@@ -123,65 +130,10 @@ def aggregate_loss(
     batch_size = targets.shape[0]
     loss = 0
     for n in range(batch_size):
-        loss_indices = recorded_neuron_indices[n] + behavior_indices
+        loss_indices = recorded_neuron_indices[n]
         loss += reconstruction_loss(targets[n, loss_indices, :],
                                     outputs[n, loss_indices, :])
-
     return loss
-
-
-def aggregate_loss_v1(
-        targets,
-        outputs,
-        recorded_neuron_indices,
-        behavior_indices,
-        mask_indices,
-        reconstruction_loss
-):
-    """ Compute MSE loss for each batch separately on the reconstruction of masked
-    neural/behavioral activities if they are recorded. """
-
-    batch_size = targets.shape[0]
-    loss = 0
-    for n in range(batch_size):
-        # get column indices that contribute to the reconstruction loss
-        # a mask index is included if it is recorded or it is a behavior
-        loss_indices = [
-                index for index in mask_indices[n]
-                if (index in behavior_indices or
-                index in recorded_neuron_indices[n])
-        ]
-        if len(recorded_neuron_indices[n]) < 3:
-            print(f'recorded: {recorded_neuron_indices[n]} mask: {mask_indices[n]} loss: {loss_indices} ')
-        loss += reconstruction_loss(targets[n, loss_indices, :],
-                                    outputs[n, loss_indices, :])
-
-    return loss
-
-
-def get_mask_indices(
-        num_inputs,
-        recorded_neuron_indices,
-        neuron_indices,
-        num_mask_indices=3):
-    """
-    Selects three columns to mask out acccording to the following scheme -
-    FOR NEURONS and BEHAVIORS
-        * Automatically masking out the missing neurons (maximum possible is 2);
-        * Randomly select (3 - # missing neurons) columns to mask out
-    """
-    mask_indices = {}
-    all_indices = list(range(num_inputs))
-
-    for n in recorded_neuron_indices.keys():
-        neuron_mask_indices = list(set(neuron_indices) - set(recorded_neuron_indices[n]))
-        remaining_indices = list(set(all_indices) - set(neuron_mask_indices))
-        num_to_be_masked = num_mask_indices - len(neuron_mask_indices)
-        rest_mask_indices = np.random.choice(remaining_indices, num_to_be_masked,
-                                             replace=False)
-        mask_indices[n] = neuron_mask_indices + rest_mask_indices.tolist()
-
-    return mask_indices
 
 
 def get_recorded_neuron_indices(targets, num_neurons):
@@ -200,10 +152,10 @@ def get_recorded_neuron_indices(targets, num_neurons):
 
 if __name__ == '__main__':
 
-    device = "cuda:2"
+    device = "cuda:3"
     window_size = 400
     window_stride = 1
-    ds_name = 'AVA_MC_SMDV_all'
+    ds_name = 'interneuron_all'
     batch_size = 32
     base = '/home/alicia/store1/alicia/attention_predict'
 
@@ -215,25 +167,26 @@ if __name__ == '__main__':
         device=device,
         slices=slice(0, 1600)
     )
-    num_iterations = 10000 #1_000_000
+    num_iterations = 1000 #1_000_000
     num_epochs = 10000
     learning_rate = 1e-4
-    exp_name = 'exp_2024110700'
+    exp_name = 'exp_2024111303'
     log_directory = f'{base}/{exp_name}'
     log_ckpt_freq = 10
     random_seed = 1912 # Alan Turing's birth year :)
 
     depth = 5
-    num_inputs = 6
-    num_neurons = 3
+    num_inputs = 14
+    num_neurons = 11
     num_behaviors = 3
+    attention_scheme = 'NfromN'
 
     model = HybridNet(
             depth,
             num_neurons,
             num_behaviors,
             window_size,
-            attention_scheme='NfromB',
+            attention_scheme=attention_scheme,
             device=device)
     train(
         num_neurons,
