e1987884965bea4a526b89832b879d2b90b93170
Alicia <lualicia88@gmail.com>
2025-01-28 22:10:41 +0000

    Test mini attention model.

--- tests/test_attention_model.py
+++ tests/test_attention_model.py
@@ -4,24 +4,26 @@ import torch
 
 if __name__ == '__main__':
 
-    depth = 3
-    num_neurons = 3
+    depth = 4
+    num_neurons = 4
     num_behaviors = 3
-    num_inputs = num_neurons + num_behaviors
     window_size = 400
     batch_size = 2
+    attention_scheme = 'BfromN'
     device = 'cuda:3'
+    num_fmaps = 16
     # (batch, channels, height, width)
-    x = torch.rand(batch_size, num_inputs, window_size).to(device)
-    model = attention_predict.AttentionModel2(
+    x = torch.rand(batch_size, num_neurons, window_size).to(device)
+    model = attention_predict.AttentionModelMini(
             depth,
             num_neurons,
             num_behaviors,
+            attention_scheme,
             window_size,
-            attention_scheme='NfromB',
+            num_fmaps=num_fmaps,
             device=device)
     y, level_attn_weights = model(x)
     print(f'outputs dim: {y.shape}')
     for attn_weights in level_attn_weights:
-        print(f'weights dim: {attn_weights.shape}')
+        print(f'attention weights: {attn_weights.shape}')
         print(attn_weights)
